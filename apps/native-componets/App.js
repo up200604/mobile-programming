cf92340cb64005fe2777e618882a8c251247aed6
Rogelio Trejo Estrada <rogelio.trejo@upa.edu.mx>
2023-10-24 09:39:27 -0600

    Add a compilation file

--- apps/native-componets/App.js
+++ apps/native-componets/App.js
@@ -1,20 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
+import { useState } from 'react';
+
+import * as ImagePiker from 'expo-image-picker';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [imageUri, setImageUri] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK2nG24AYDm6FOEC7jIfgubO96GbRso2Xshu1f8abSYQ&s");
+
+
+    const handdlePress = async () => {
+        const permitions = await ImagePiker.requestCameraPermissionsAsync();
+
+        if (!permitions.granted) {
+            Alert.alert("This is bloquet!!!!!!!")
+        }
+
+        const result = await ImagePiker.launchCameraAsync()
+        const [camera] = result.assets;
+
+        setImageUri(camera)
+    }
+
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text>Select an image</Text>
+                <Image
+                    style={{ width: 250, height: 250 }}
+                    source={{
+                        uri: imageUri
+                    }}
+                />
+                <Button
+                    title='Click ME!!!'
+                    onPress={handdlePress}
+                />
+            </View>
+
+
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
